3a11efd59269709fb5da2099035075acf516c455
ggdoe <gregoire.doebele@ens.uvsq.fr>
2021-12-12 19:13:37 +0100

    3.4.1 integrator.h

--- src/Integrator.h
+++ src/Integrator.h
@@ -6,7 +6,10 @@
 class Integrator {
     Integrator() = delete;
     ~Integrator() = delete;
-    static void update(double t, double d_t, Variable &v);
+    template<class Lambda>
+    static void update(double t, double d_t, Variable &v, Lambda f);
+    // pourquoi https://en.cppreference.com/w/cpp/algorithm/remove
+    // lambda passé sans référerence ?
 };
 
 #endif
